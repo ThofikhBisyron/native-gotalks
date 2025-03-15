aa4eea2ac2dc40ab1922535797b68a76da78d45a
ThofikhBisyron <sanada.ryuzaki1@gmail.com>
2025-03-16 04:58:58 +0700

    React Native With TailwindCSS

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+import "../global.css"
+
+export default function Index() {
+  return (
+    <View>
+      <Text>Edit app/index.tsx to edit this screen. hello</Text>
+      <View className="text-red-700">
+        <Text className="text-red-700 text-4xl">Hellos</Text>
+      </View>
+      <View className="text-red-700 w-full">
+        <Text>Hellsaa</Text>
+      </View>
+    </View>
+  );
+}

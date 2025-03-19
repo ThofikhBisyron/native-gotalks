7dc0eb626fc0eae803e74553fdd75f12ebc0ac99
RaisyaSyaifania <syaifania.raisya@gmail.com>
2025-03-19 08:36:06 +0700

    feat: add development build, DropDownPicker, SelectList, Picker

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,12 @@
 import { Stack } from "expo-router";
+import { View, useColorScheme } from "react-native";
+import "../global.css"
 
 export default function RootLayout() {
-  return <Stack />;
+  const theme = useColorScheme()
+  return (
+    <View className={theme === "dark" ? "flex-1 bg-black" : "flex-1 bg-white"}>
+      <Stack screenOptions={{ headerShown: false}}/>
+    </View>    
+  )
 }

68b93922e21bb0e84abddc5e86cb4074c4e8793a
ThofikhBisyron <sanada.ryuzaki1@gmail.com>
2025-04-05 01:25:02 +0700

    feat: fix send messages in chat screen

--- app/chat/chat.tsx
+++ app/chat/chat.tsx
@@ -16,6 +16,7 @@ export default function Chatscreen() {
         image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTL1D8_YIJm9XpGyo81GbGPIYFGhFrBKhs0TA&s",
       };
 
+    const [inputtext, setInputText] = useState(40)
     const [messages, setMessages] = useState([
         {
             id: 1,
@@ -32,6 +33,8 @@ export default function Chatscreen() {
             type: "text",
         }
     ])
+
+    const reverseMessage = [...messages].reverse()
     
     return (
         <View className="flex-1">
@@ -40,25 +43,33 @@ export default function Chatscreen() {
         image={contacts.image}
         />  
         <FlatList
-        data = {messages.reverse()}
+        data = {reverseMessage}
         inverted
         renderItem={({item}) =>(
             <View className={`my-1 ${item.isme ? "items-end" : "items-start"}`}>
                 <View className={`rounded-xl px-3 py-3 ${item.isme ? "bg-green-300" : "bg-slate-300"}`}>
-                    <Text>{item.message}</Text>
+                    <Text className="text-xl">{item.message}</Text>
                 </View>
             </View>
         )}  
         />
         <View className="flex flex-row justify-between mr-3 ml-3">
-            <TextInput mode="outlined" outlineColor="transparent" 
-            multiline={false} 
-            numberOfLines={3} 
+            <TextInput mode="outlined" outlineColor="transparent" activeOutlineColor="transparent" 
+            multiline={true} 
+            numberOfLines={4} 
             placeholder="Type a message"
             textAlignVertical="top" 
+            scrollEnabled={true}
+            onContentSizeChange={(event) =>{
+                setInputText(Math.min(120, event.nativeEvent.contentSize.height))
+            }}
+            style={[
+                {height: inputtext, minHeight:40, maxHeight:120},
+                {fontSize: 16}
+            ]}
             className="rounded-3xl w-96 bg-slate-300"
             />
-            <TouchableOpacity className="items-center justify-center">
+            <TouchableOpacity className="flex-1 items-center justify-center">
                 <MaterialIcons name="send" size={40}/>
             </TouchableOpacity>
         </View>

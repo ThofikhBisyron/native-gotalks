f6c745df507ff55a6f45aa0cbf539ca356d67645
ThofikhBisyron <sanada.ryuzaki1@gmail.com>
2025-07-22 03:19:40 +0700

    feat: fix opening screen

--- app/opening.tsx
+++ app/opening.tsx
@@ -1,10 +1,12 @@
 import { Text, View } from "react-native"
 import { MotiText } from "moti";
+import { useSelector } from "react-redux";
+import { RootState } from "@/redux/store";
+import { useEffect } from "react";
 
 export default function Opening() {
+   
 
-    
-    
     return(
     <View className="flex-1 justify-center items-center">
         <MotiText from={{ opacity: 1, scale:0.1 }}
